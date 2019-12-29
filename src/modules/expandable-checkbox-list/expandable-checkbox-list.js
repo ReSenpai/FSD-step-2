668559d549e4c18f5d6e093af2516c4fa2cc3895
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2019-12-29 09:38:02 +0300

    expandable-checkbox 99%

--- src/modules/expandable-checkbox-list/expandable-checkbox-list.js
+++ src/modules/expandable-checkbox-list/expandable-checkbox-list.js
@@ -3,6 +3,19 @@ import $ from 'jquery';
 $(document).ready(function(){
 
     $('.checkbox-list').on('click',function(){
-        $('.checkbox-list__ul').slideToggle(350);
-    });
+        $('.checkbox-list__ul').slideToggle(300);
+
+        var arrow = $('.checkbox-list__arrow').text();
+        
+        switch(arrow) {
+            case 'expand_less':
+                arrow = 'expand_more';
+                break;
+            case 'expand_more':
+                arrow = 'expand_less';
+                break;
+        }
+
+        $('.checkbox-list__arrow').text(arrow);
+    });   
 });

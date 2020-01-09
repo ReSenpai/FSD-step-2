0989b8a80987a8220cc5ca173e625bc582ba42bb
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-01-09 18:35:02 +0300

    Reviews-box 99%
    Сделал блок с отзывами, так же переписал немного логику кнопки лайков, что бы работало при множественнмо количестве.

--- src/modules/like-button/like-button.js
+++ src/modules/like-button/like-button.js
@@ -3,20 +3,20 @@ import $ from 'jquery';
 $(document).ready(function() {
 
     $('.like-button').on('click',function(){
-        var counter = parseInt($('.like-button__counter').text());
+        var counter = parseInt($(this).find('.like-button__counter').text());
 
-        var checked = $('.like-button').hasClass('like-button_active');
+        var checked = $(this).hasClass('like-button_active');
 
         switch(checked) {
             case true:
-                $('.like-button__counter').text(counter - 1).removeClass('like-button__counter_active');
-                $('.like-button__icon').text('favorite_border').removeClass('like-button__icon_active');
-                $('.like-button').removeClass('like-button_active');
+                $(this).find('.like-button__counter').text(counter - 1).removeClass('like-button__counter_active');
+                $(this).find('.like-button__icon').text('favorite_border').removeClass('like-button__icon_active');
+                $(this).removeClass('like-button_active');
                 break;
             case false:
-                $('.like-button__counter').text(counter + 1).addClass('like-button__counter_active');
-                $('.like-button__icon').text('favorite').addClass('like-button__icon_active');
-                $('.like-button').addClass('like-button_active');
+                $(this).find('.like-button__counter').text(counter + 1).addClass('like-button__counter_active');
+                $(this).find('.like-button__icon').text('favorite').addClass('like-button__icon_active');
+                $(this).addClass('like-button_active');
                 break;
         }
     });
